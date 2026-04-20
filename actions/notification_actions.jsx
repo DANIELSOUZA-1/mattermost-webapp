420343cc52aba284f55a56924ace04b6f6fc8f67
Daniel Antônio <66747998+DANIELSOUZA-1@users.noreply.github.com>
2026-04-20 09:26:22 -0300

    Revert "Revert "function sendDesktopNotification refactoring""
    This reverts commit 48f6c078552cdac8745e7e9e95f5c0ab99c73558.

--- actions/notification_actions.jsx
+++ actions/notification_actions.jsx
@@ -33,165 +33,138 @@ export function sendDesktopNotification(post, msgProps) {
         const state = getState();
         const currentUserId = getCurrentUserId(state);
 
-        if ((currentUserId === post.user_id && post.props.from_webhook !== 'true')) {
+        if (shouldSkipNotification(currentUserId, post, state, msgProps)) {
             return;
         }
 
-        if (isSystemMessage(post) && !isUserAddedInChannel(post, currentUserId)) {
-            return;
-        }
-
-        let userFromPost = getUser(state, post.user_id);
-        if (!userFromPost) {
-            const missingProfileResponse = await dispatch(getProfilesByIds([post.user_id]));
-            if (missingProfileResponse.data && missingProfileResponse.data.length) {
-                userFromPost = missingProfileResponse.data[0];
-            }
-        }
-
-        let mentions = [];
-        if (msgProps.mentions) {
-            mentions = JSON.parse(msgProps.mentions);
-        }
-
-        let followers = [];
-        if (msgProps.followers) {
-            followers = JSON.parse(msgProps.followers);
-            mentions = [...new Set([...followers, ...mentions])];
-        }
-
-        const teamId = msgProps.team_id;
-
-        let channel = makeGetChannel()(state, {id: post.channel_id});
-        const user = getCurrentUser(state);
-        const userStatus = getStatusForUserId(state, user.id);
-        const member = getMyChannelMember(state, post.channel_id);
-        const isCrtReply = isCollapsedThreadsEnabled(state) && post.root_id !== '';
-
-        if (!member || isChannelMuted(member) || userStatus === UserStatuses.DND || userStatus === UserStatuses.OUT_OF_OFFICE) {
-            return;
-        }
-
-        let notifyLevel = member?.notify_props?.desktop || NotificationLevels.DEFAULT;
-
-        if (notifyLevel === NotificationLevels.DEFAULT) {
-            notifyLevel = user?.notify_props?.desktop || NotificationLevels.ALL;
-        }
-
-        if (notifyLevel === NotificationLevels.NONE) {
-            return;
-        } else if (notifyLevel === NotificationLevels.MENTION && mentions.indexOf(user.id) === -1 && msgProps.channel_type !== Constants.DM_CHANNEL) {
-            return;
-        } else if (isCrtReply && notifyLevel === NotificationLevels.ALL && followers.indexOf(currentUserId) === -1) {
-            // if user is not following the thread don't notify
-            return;
-        }
-
-        const config = getConfig(state);
-        let username = '';
-        if (post.props.override_username && config.EnablePostUsernameOverride === 'true') {
-            username = post.props.override_username;
-        } else if (userFromPost) {
-            username = displayUsername(userFromPost, getTeammateNameDisplaySetting(state), false);
-        } else {
-            username = Utils.localizeMessage('channel_loader.someone', 'Someone');
-        }
-
-        let title = Utils.localizeMessage('channel_loader.posted', 'Posted');
-        if (!channel) {
-            title = msgProps.channel_display_name;
-            channel = {
-                name: msgProps.channel_name,
-                type: msgProps.channel_type,
-            };
-        } else if (channel.type === Constants.DM_CHANNEL) {
-            title = Utils.localizeMessage('notification.dm', 'Direct Message');
-        } else {
-            title = channel.display_name;
-        }
-
-        if (title === '') {
-            if (msgProps.channel_type === Constants.DM_CHANNEL) {
-                title = Utils.localizeMessage('notification.dm', 'Direct Message');
-            } else {
-                title = msgProps.channel_display_name;
-            }
-        }
-
-        if (isCrtReply) {
-            title = Utils.localizeAndFormatMessage(t('notification.crt'), 'Reply in {title}', {title});
-        }
-
-        let notifyText = post.message;
-
-        const msgPropsPost = JSON.parse(msgProps.post);
-        const attachments = msgPropsPost && msgPropsPost.props && msgPropsPost.props.attachments ? msgPropsPost.props.attachments : [];
-        let image = false;
-        attachments.forEach((attachment) => {
-            if (notifyText.length === 0) {
-                notifyText = attachment.fallback ||
-                    attachment.pretext ||
-                    attachment.text;
-            }
-            image |= attachment.image_url.length > 0;
-        });
-
-        let strippedMarkdownNotifyText = stripMarkdown(notifyText);
-
-        const notifyTextMaxLength = isWindowsApp() ? WINDOWS_NOTIFY_TEXT_MAX_LENGTH : NOTIFY_TEXT_MAX_LENGTH;
-        if (strippedMarkdownNotifyText.length > notifyTextMaxLength) {
-            strippedMarkdownNotifyText = strippedMarkdownNotifyText.substring(0, notifyTextMaxLength - 1) + '...';
-        }
-
-        let body = `@${username}`;
-        if (strippedMarkdownNotifyText.length === 0) {
-            if (msgProps.image) {
-                body += Utils.localizeMessage('channel_loader.uploadedImage', ' uploaded an image');
-            } else if (msgProps.otherFile) {
-                body += Utils.localizeMessage('channel_loader.uploadedFile', ' uploaded a file');
-            } else if (image) {
-                body += Utils.localizeMessage('channel_loader.postedImage', ' posted an image');
-            } else {
-                body += Utils.localizeMessage('channel_loader.something', ' did something new');
-            }
-        } else {
-            body += `: ${strippedMarkdownNotifyText}`;
-        }
-
-        //Play a sound if explicitly set in settings
-        const sound = !user.notify_props || user.notify_props.desktop_sound === 'true';
-
-        // Notify if you're not looking in the right channel or when
-        // the window itself is not active
-        const activeChannel = getCurrentChannel(state);
-        const channelId = channel ? channel.id : null;
-
-        let notify = false;
-        if (isCrtReply) {
-            notify = !isThreadOpen(state, post.root_id);
-        } else {
-            notify = activeChannel && activeChannel.id !== channelId;
-        }
-        notify = notify || !state.views.browser.focused;
-
-        const soundName = user.notify_props !== undefined && user.notify_props.desktop_notification_sound !== undefined ? user.notify_props.desktop_notification_sound : 'Bing';
-
-        if (notify) {
-            const updatedState = getState();
-            let url = Utils.getChannelURL(updatedState, channel, teamId);
-
-            if (isCrtReply) {
-                url = Utils.getPermalinkURL(updatedState, teamId, post.id);
-            }
-
-            dispatch(notifyMe(title, body, channel, teamId, !sound, soundName, url));
-
-            //Don't add extra sounds on native desktop clients
-            if (sound && !isDesktopApp() && !isMobileApp()) {
-                Utils.ding(soundName);
-            }
+        const notifyLevel = determineNotifyLevel(state, post, msgProps, currentUserId);
+        if (shouldNotify(notifyLevel, msgProps, currentUserId, state)) {
+            const { title, body, soundName, url } = await prepareNotificationData(post, msgProps, state);
+            dispatchNotification(dispatch, title, body, soundName, url, state);
         }
     };
+}
+
+function shouldSkipNotification(currentUserId, post, state, msgProps) {
+    return (
+        (currentUserId === post.user_id && post.props.from_webhook !== 'true') ||
+        (isSystemMessage(post) && !isUserAddedInChannel(post, currentUserId)) ||
+        shouldSkipBasedOnChannelAndUser(state, post, currentUserId)
+    );
+}
+
+function shouldSkipBasedOnChannelAndUser(state, post, currentUserId) {
+    const userStatus = getStatusForUserId(state, currentUserId);
+    const member = getMyChannelMember(state, post.channel_id);
+    return (
+        !member || 
+        isChannelMuted(member) || 
+        userStatus === UserStatuses.DND || 
+        userStatus === UserStatuses.OUT_OF_OFFICE
+    );
+}
+
+function determineNotifyLevel(state, post, msgProps, currentUserId) {
+    let notifyLevel = getMyChannelMember(state, post.channel_id)?.notify_props?.desktop || NotificationLevels.DEFAULT;
+    if (notifyLevel === NotificationLevels.DEFAULT) {
+        notifyLevel = getCurrentUser(state)?.notify_props?.desktop || NotificationLevels.ALL;
+    }
+    return notifyLevel;
+}
+
+function shouldNotify(notifyLevel, msgProps, currentUserId, state) {
+    if (notifyLevel === NotificationLevels.NONE) {
+        return false;
+    } else if (notifyLevel === NotificationLevels.MENTION && !msgProps.mentions.includes(currentUserId)) {
+        return false;
+    } else if (notifyLevel === NotificationLevels.ALL && !isThreadOpen(state, msgProps.root_id)) {
+        return false;
+    }
+    return true;
+}
+
+async function prepareNotificationData(post, msgProps, state) {
+    let userFromPost = await fetchUserFromPost(state, post);
+    const channel = makeGetChannel()(state, { id: post.channel_id }) || { name: msgProps.channel_name, type: msgProps.channel_type };
+    const title = generateTitle(channel, msgProps);
+    const notifyText = generateNotifyText(post, msgProps, state, userFromPost);
+    const body = generateBody(notifyText, post, msgProps, state, userFromPost);
+    const soundName = getSoundName(state);
+    const url = Utils.getChannelURL(state, channel, msgProps.team_id);
+    return { title, body, soundName, url };
+}
+
+async function fetchUserFromPost(state, post) {
+    let userFromPost = getUser(state, post.user_id);
+    if (!userFromPost) {
+        const missingProfileResponse = await dispatch(getProfilesByIds([post.user_id]));
+        if (missingProfileResponse.data && missingProfileResponse.data.length) {
+            userFromPost = missingProfileResponse.data[0];
+        }
+    }
+    return userFromPost;
+}
+
+function generateNotifyText(post, msgProps, state, userFromPost) {
+    let notifyText = post.message;
+    const msgPropsPost = JSON.parse(msgProps.post);
+    const attachments = msgPropsPost?.props?.attachments || [];
+
+    attachments.forEach((attachment) => {
+        if (!notifyText.length) {
+            notifyText = attachment.fallback || attachment.pretext || attachment.text;
+        }
+    });
+
+    let strippedMarkdownNotifyText = stripMarkdown(notifyText);
+    const notifyTextMaxLength = isWindowsApp() ? WINDOWS_NOTIFY_TEXT_MAX_LENGTH : NOTIFY_TEXT_MAX_LENGTH;
+
+    if (strippedMarkdownNotifyText.length > notifyTextMaxLength) {
+        strippedMarkdownNotifyText = `${strippedMarkdownNotifyText.substring(0, notifyTextMaxLength - 1)}...`;
+    }
+
+    return strippedMarkdownNotifyText;
+}
+
+function generateBody(notifyText, post, msgProps, state, userFromPost) {
+    let body = `@${getUsername(post, state, userFromPost)}`;
+    if (!notifyText.length) {
+        body += getUploadMessage(msgProps, post);
+    } else {
+        body += `: ${notifyText}`;
+    }
+    return body;
+}
+
+function getUsername(post, state, userFromPost) {
+    const config = getConfig(state);
+    if (post.props.override_username && config.EnablePostUsernameOverride === 'true') {
+        return post.props.override_username;
+    } else if (userFromPost) {
+        return displayUsername(userFromPost, getTeammateNameDisplaySetting(state), false);
+    } else {
+        return Utils.localizeMessage('channel_loader.someone', 'Someone');
+    }
+}
+
+function getUploadMessage(msgProps, post) {
+    if (msgProps.image) {
+        return Utils.localizeMessage('channel_loader.uploadedImage', ' uploaded an image');
+    } else if (msgProps.otherFile) {
+        return Utils.localizeMessage('channel_loader.uploadedFile', ' uploaded a file');
+    } else if (post.props.attachments?.some((attachment) => attachment.image_url.length > 0)) {
+        return Utils.localizeMessage('channel_loader.postedImage', ' posted an image');
+    } else {
+        return Utils.localizeMessage('channel_loader.something', ' did something new');
+    }
+}
+
+function dispatchNotification(dispatch, title, body, soundName, url, state) {
+    const notify = !state.views.browser.focused;
+    dispatch(notifyMe(title, body, soundName, url, notify));
+    if (!isDesktopApp() && !isMobileApp()) {
+        Utils.ding(soundName);
+    }
 }
 
 const notifyMe = (title, body, channel, teamId, silent, soundName, url) => (dispatch) => {
